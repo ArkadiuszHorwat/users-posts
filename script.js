fb3032ac09ae3b9b6195cb57ec357e0e01adcaa4
unknown <arkadiuszhorwat@gmail.com>
2021-04-12 17:10:15 +0200

    create setPostsToUsers function

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('Hello');
+const axios = require('axios');
+
+const getPosts = async () => {
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        return response.data;
+    }catch(errors){
+        console.error(errors);
+    }
+}
+
+const getUsers = async () => {
+    try{
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        return response.data;
+    }catch(errors){
+        console.error(errors);
+    }
+}
+
+const setPostsToUsers = async () => {
+    const usersTab = await getUsers();
+    const postsTab = await getPosts();
+
+    usersTab.map(user => {
+        let posts = [];
+
+        postsTab.map(post => {
+            user.id === post.userId && posts.push(post);
+        });
+
+        user.posts = posts;
+    });
+}
+
+setPostsToUsers();
