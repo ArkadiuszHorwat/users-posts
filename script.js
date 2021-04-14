52fdd25b5aeac2118ba29fbf80629f6d82703602
unknown <arkadiuszhorwat@gmail.com>
2021-04-14 16:57:58 +0200

    create findClosestUser function

--- script.js
+++ script.js
@@ -40,7 +40,10 @@ const countPosts = async () => {
     const strList = [];
 
     users.map(user => {
-        strList.push(`${user.name} napisał(a) ${user.posts.length} postów.`);
+        if(user.posts.length){
+            strList.push(`${user.name} napisał(a) ${user.posts.length} postów.`);
+        }
+        else strList.push(`${user.name} nie napisał(a) żadnych postów.`)
     });
 
     return strList;
@@ -61,8 +64,59 @@ const checkUniqTitles = async () => {
         titles[i] != current ? current = titles[i] : duplicates.push(titles[i]);
     }
 
-    return duplicates;
+    return duplicates.length ? duplicates : 'Wszystkie tytuły są unikalne.';
 }
 
-countPosts().then(response => console.log('ilość postów: ',response));
-checkUniqTitles().then(response => console.log('duplikaty: ', response));
+const findClosestUser = async () => {
+    const users = await getUsers();
+
+    users.map(user => {
+        const firstLat = user.address.geo.lat;
+        const firstLng = user.address.geo.lng;
+        let minDistance = null;
+        let closestUser;
+
+        users.map(otherUser => {
+            if(otherUser.id !== user.id){
+                const secondLat = otherUser.address.geo.lat;
+                const secondLng = otherUser.address.geo.lng;
+                let tmpDistance = getDistance(firstLat,firstLng,secondLat,secondLng);
+
+                if(minDistance === null){
+                    minDistance = tmpDistance;
+                    closestUser = otherUser.name;
+                }
+                else if(minDistance > tmpDistance){
+                    minDistance = tmpDistance;
+                    closestUser = otherUser.name;
+                }
+            }
+        })
+        console.log(`${user.name} jest najbliżej użytkownika ${closestUser}. Oddaleni są od siebie o ${Math.round(minDistance)}km`);
+    })
+}
+
+//geo distance in km between two points
+function getDistance(firstLat,firstLng,secondLat,secondLng) {
+    const R = 6371;
+    const dLat = convertToRad(secondLat-firstLat);
+    const dLon = convertToRad(secondLng-firstLng);
+
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(convertToRad(firstLat)) * Math.cos(convertToRad(secondLat)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = R * c;
+
+    return d;
+  }
+  
+  function convertToRad(deg) {
+    return deg * (Math.PI/180);
+  }
+
+countPosts().then(response => console.log('Ilość postów użytkowników: ',response));
+checkUniqTitles().then(response => console.log('Powtarzające się tytuły postów: ', response));
+findClosestUser().then(response => console.log('Użytkownicy mieszkający najbliżej siebie: ', response));
